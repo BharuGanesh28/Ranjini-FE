32fcf41e86131026f256ce8cd8ce5cf8ce4f4b04
Bharath1503 <bharathganesh1503@gmail.com>
2023-04-17 20:00:03 +0530

    portfolio

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import ResponsiveAppBar from './components/appbar';
+import Portfolio from './components/portfolio';
 function App() {
   return (
     <div className="App">
       <ResponsiveAppBar/>
+      <Portfolio/>
     </div>
   );
 }
