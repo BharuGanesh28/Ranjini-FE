93efb7616e2f62c836a197e047a54f4ab7b06d9a
BharuGanesh28 <bharuganesh283@gmail.com>
2023-04-17 20:12:10 +0530

    to resolve conflict

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import logo from './logo.svg';
 import './App.css';
 import ResponsiveAppBar from './components/appbar';
 import About from './components/aboutus';
+import Portfolio from './components/portfolio';
 function App() {
   return (
     <div className="App">
       <ResponsiveAppBar/>
       <About/>
+      <Portfolio/>
     </div>
   );
 }
