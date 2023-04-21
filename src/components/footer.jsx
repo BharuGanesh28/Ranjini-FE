ebf236366498a51679b1b0bde7be393372f53a27
BharuGanesh28 <bharuganesh283@gmail.com>
2023-04-21 14:14:37 +0530

    resolve conflict

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -14,8 +14,7 @@ return(
     backgroundColor:'#3a3a3a',
     display:'flex',
     justifyContent:'space-evenly',
-    padding:'30px'
-    
+    padding:'30px'    
 }} sx={{color:"primary.main",alignItems:{xs:"center"}}}  id="CONTACT">
     <Grid item xs={10} sm={5} md={2}  style={{justifyContent:'centre'}}> 
     <Grid container sx={{color:"primary.main"}} style={{display:'flex',justifyContent:'space-evenly'}} >
